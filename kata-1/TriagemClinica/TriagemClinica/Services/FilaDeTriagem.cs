7aa927c9e523b745d1f9c5d015fcd30491b7e5f9
Rafazg <claudenirorafaelbjj@gmail.com>
2026-04-21 00:53:47 -0300

    feat: Implementa serviço FilaDeTriagem kata-1

--- kata-1/TriagemClinica/TriagemClinica/Services/FilaDeTriagem.cs
+++ kata-1/TriagemClinica/TriagemClinica/Services/FilaDeTriagem.cs
@@ -1,12 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using TriagemClinica.Models;
 
-namespace TriagemClinica.Services
+namespace TriagemClinica.Services;
+
+public class FilaDeTriagem
 {
-    internal class FilaDeTriagem
+    public IReadOnlyList<Paciente> OrdenarFila(IEnumerable<Paciente> pacientes)
     {
+        if (pacientes is null)
+            throw new ArgumentNullException(nameof(pacientes));
+
+        return pacientes
+            .OrderBy(p => (int)p.UrgenciaEfetiva)
+            .ThenBy(p => p.HorarioChegada)
+            .ToList();
     }
 }
